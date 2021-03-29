ac7b8c7c16b69134678d904a66d4eb3316bbe6c7
cwsoft <info@cwsoft.de>
2021-03-29 17:45:22 +0200

    Added error handling to profile.ps

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,25 +1,33 @@
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+ PowerShell 5.x user profile
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
+# User defined alias.
+Set-Alias -Name touch -Value New-Item
+
 # Work out if PowerShell was started with Admin rights.
-$fgColor = "Green"
-if ($host.UI.RawUI.WindowTitle -match "Administrator") {$fgColor = "DarkRed"}
+if ($host.UI.RawUI.WindowTitle -match "Administrator") {$fgColor = "DarkRed"} else {$fgColor = "Green"}
 
 function prompt {
-    # Write Prompt with Path and optional Git branch and command prompt in new line.
-    Write-Host "$(Get-Location) " -ForegroundColor $fgColor -NoNewline
-    (GetGitBranchName)
+    # Create multiline command prompt with current path and optional git branch name and status.
+    Write-Host "$(Get-Location)" -ForegroundColor $fgColor -NoNewline
+    (outputGitBranchName)
+    # Output Unicode "Lambda" char into new line as default prompt.
     Write-Host "`n$([char]0x03BB)" -ForegroundColor White -NoNewline
     return " "
 }
 
-function GetGitBranchName {
-    # Work out branch name and status if current folder is a valid git repo.
-    $currentBranch = (git branch --show-current *2 $null)
-    if ([string]::IsNullOrEmpty($currentBranch)) {return " "}
-
-    # Check if git index of actual branch and working tree are clean.
-    $fgColor = "White"
-    if ((git status --porcelain)) { $fgColor = "DarkRed"}
-    Write-Host "($currentBranch)" -ForegroundColor $fgColor -NoNewline
+function outputGitBranchName {
+    Try {
+        # Try to extract git branch if current path is a valid git repo.
+        $currentBranch = (git branch --show-current 2>$null)
+        if ($currentBranch) {
+            # Check if index and working tree are clean.
+            if ((git status --porcelain)) { $fgColor = "DarkRed"} else { $fgColor = "White"}
+            # Indicate if branch is clean by setting branch name color to white (clean) or red.
+            Write-Host " ($currentBranch)" -ForegroundColor $fgColor -NoNewline
+        }
+    } Catch {
+        # Skip on errors (e.g. git not installed/accessible, insufficient permissions ...).
+        return " "
+    }
 }
