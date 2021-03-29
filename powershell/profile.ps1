8fcfc182ad60a3a47512b71665655fcc96043f90
cwsoft <info@cwsoft.de>
2021-03-29 17:58:04 +0200

    Updated settings. Renamed some variables.

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -5,11 +5,11 @@
 Set-Alias -Name touch -Value New-Item
 
 # Work out if PowerShell was started with Admin rights.
-if ($host.UI.RawUI.WindowTitle -match "Administrator") {$fgColor = "DarkRed"} else {$fgColor = "Green"}
+if ($host.UI.RawUI.WindowTitle -match "Administrator") {$pathColor = "DarkRed"} else {$pathColor = "Green"}
 
 function prompt {
     # Create multiline command prompt with current path and optional git branch name and status.
-    Write-Host "$(Get-Location)" -ForegroundColor $fgColor -NoNewline
+    Write-Host "$(Get-Location)" -ForegroundColor $pathColor -NoNewline
     (outputGitBranchName)
     # Output Unicode "Lambda" char into new line as default prompt.
     Write-Host "`n$([char]0x03BB)" -ForegroundColor White -NoNewline
@@ -22,9 +22,9 @@ function outputGitBranchName {
         $currentBranch = (git branch --show-current 2>$null)
         if ($currentBranch) {
             # Check if index and working tree are clean.
-            if ((git status --porcelain)) { $fgColor = "DarkRed"} else { $fgColor = "White"}
+            if ((git status --porcelain)) { $branchColor = "DarkRed"} else { $branchColor = "White"}
             # Indicate if branch is clean by setting branch name color to white (clean) or red.
-            Write-Host " ($currentBranch)" -ForegroundColor $fgColor -NoNewline
+            Write-Host " ($currentBranch)" -ForegroundColor $branchColor -NoNewline
         }
     } Catch {
         # Skip on errors (e.g. git not installed/accessible, insufficient permissions ...).
